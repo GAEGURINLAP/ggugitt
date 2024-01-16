36ca206cebf42d7822fecb0601adff74c4543191
lbo728 <extreme93@naver.com>
2024-01-16 09:48:14 +0900

    fix: helmet provider 일단 보류

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { HelmetProvider } from "react-helmet-async";
 
-import App from "./App.tsx";
+import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HelmetProvider>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // <HelmetProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  // </HelmetProvider>
 );
