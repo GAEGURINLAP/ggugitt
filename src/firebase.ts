dd9ca5dc30465feefed7852c2549f4d46e93b25f
lbo728 <extreme93@naver.com>
2024-01-21 00:38:07 +0900

    setting: firebase 환경설정 수정

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,19 +4,21 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB4nSKNemv46Pnj_k5lz83zxrUeWk_0_L0",
-  authDomain: "bullgaemi-survey.firebaseapp.com",
-  projectId: "bullgaemi-survey",
-  storageBucket: "bullgaemi-survey.appspot.com",
-  messagingSenderId: "441940389851",
-  appId: "1:441940389851:web:972dabf13186ace4f541fe",
-  measurementId: "G-XTF0M3MMH3",
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+// measurementId: "G-XTF0M3MMH3",
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
+
+export const isPRD = import.meta.env.VITE_ENV === "prd" ? true : false;
 
 export const auth = getAuth(app);
 
