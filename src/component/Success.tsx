d7c46336d506f1d8bfb8d7bda2ced01164ff00d1
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-02-17 02:45:02 +0900

    feature: Success -> 투표 등록 완료 & 투표 성공 때 피드백 링크 버튼

--- src/component/Success.tsx
+++ src/component/Success.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { ButtonWrapper, Icon, Text, Wrapper } from "../style/success";
 import ButtonPrimary from "./ButtonPrimary";
 import ButtonSecondary from "./ButtonSecondary";
+import ButtonThird from "./ButtonThird";
 
 interface SuccessProps {
   message: string;
@@ -19,6 +20,11 @@ export default function Success({
   onClick,
 }: SuccessProps) {
   const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    window.location.href =
+      "https://smoggy-cross-906.notion.site/ggugitt-b45750f76a824c4bb939cd57332356c2";
+  };
   return (
     <Wrapper>
       <Icon>
@@ -31,16 +37,18 @@ export default function Success({
       </Icon>
       <Text>{message}</Text>
       {isShowButton && (
-        <ButtonWrapper>
-          {isShowSecondaryButton && (
-            <ButtonSecondary
-              label="메인으로 가기"
-              onClick={() => navigate("/")}
-            />
-          )}
-
-          <ButtonPrimary label={label} onClick={onClick} />
-        </ButtonWrapper>
+        <>
+          <ButtonWrapper>
+            {isShowSecondaryButton && (
+              <ButtonSecondary
+                label="메인으로 가기"
+                onClick={() => navigate("/")}
+              />
+            )}
+            <ButtonPrimary label={label} onClick={onClick} />
+          </ButtonWrapper>
+          <ButtonThird label={"피드백 남겨주기"} onClick={handleButtonClick} />
+        </>
       )}
     </Wrapper>
   );
