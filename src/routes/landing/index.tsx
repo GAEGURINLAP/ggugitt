d3cb30276016e6aeea32b1ece15f9d138ea30407
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-05-04 23:28:43 +0900

    design: Landing -> 모바일 버전  Section01 작업중

--- src/routes/landing/index.tsx
+++ src/routes/landing/index.tsx
@@ -1,7 +1,19 @@
-import { BrandingWrapper, GNB, Menu } from "./style";
+import {
+  BrandingWrapper,
+  GNB,
+  Menu,
+  Section01,
+  Section01Desc,
+  Section01ImgWrapper,
+  Section01Title,
+  Section01TitleWrapper,
+  Wrapper,
+} from './style';
 
-import BrandingTitleSm from "/images/logo/lg-brandingtitle-sm.png";
-import MenuButton from "/images/icon/common/icon-menu.svg";
+import BrandingTitleSm from '/images/logo/lg-brandingtitle-sm.png';
+import MenuButton from '/images/icon/common/icon-menu.svg';
+import Section01Img from '/images/illust/il-landing-section-01.webp';
+import ButtonPrimary from '../../component/ButtonPrimary';
 
 export default function Landing() {
   return (
@@ -14,12 +26,23 @@ export default function Landing() {
           <img src={MenuButton} height={24} />
         </Menu>
       </GNB>
-      <div>랜딩이 될 예정</div>
-      <div>바디가 들어갈 공간</div>
-      <div>서브 바디가 들어갈 공간</div>
-      <div>섹션이 들어갈 공간</div>
-      <div>서브섹션 들어갈 공간</div>
-      <div>푸터 들어갈 공간</div>
+      <Wrapper>
+        <Section01>
+          <Section01TitleWrapper>
+            <Section01Title>
+              끈김없는 연속적인 <br /> 투표 서비스
+            </Section01Title>
+            <Section01Desc>
+              가끔... 투표가 끊겨서 아쉬운, <br />
+              그런 때 있지 않나요?
+            </Section01Desc>
+            <ButtonPrimary label="첫 투표 시작하기" />
+          </Section01TitleWrapper>
+          <Section01ImgWrapper>
+            <img src={Section01Img} alt="히어로 이미지" width={390} />
+          </Section01ImgWrapper>
+        </Section01>
+      </Wrapper>
     </>
   );
 }
--- src/routes/landing/style.ts
+++ src/routes/landing/style.ts
@@ -7,9 +7,55 @@ export const GNB = styled.div`
   padding: 0 24px;
   align-items: center;
   width: 100%;
+  max-width: 500px;
   height: 48px;
   background-color: white;
 `;
 
 export const BrandingWrapper = styled.div``;
 export const Menu = styled.div``;
+
+export const Wrapper = styled.div`
+  margin-top: 48px;
+`;
+export const Section01 = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #d3f7f9;
+  gap: 32px;
+  height: 720px;
+`;
+
+export const Section01TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  z-index: 2;
+  align-items: center;
+`;
+
+export const Section01Title = styled.div`
+  font-size: 32px;
+  text-align: center;
+  line-height: 150%;
+  font-weight: 700;
+  color: #121212;
+`;
+
+export const Section01Desc = styled.div`
+  font-size: 20px;
+  text-align: center;
+  line-height: 150%;
+  color: #525252;
+`;
+
+export const Section01ImgWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  height: 720px;
+  z-index: 1;
+`;
